804fb595818ac043bd6149f53acada2f4f270f32
Lucas Braga <lucas23an@hotmail.com>
2017-12-21 19:18:01 -0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import { Time } from './time'
+
+require('style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+
+new Vue({
+  el: '#app',
+  data: {
+    times: [
+      new Time("America MG", require('./assets/america_mg_60x60.png')),
+      new Time("Atletico PR", require('./assets/atletico-pr_60x60.png')),
+    ]
+  }
+})
