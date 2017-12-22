ede04da197a2d5919b0bf82531220de303055561
Lucas Braga <lucas23an@hotmail.com>
2017-12-22 19:24:56 -0200

    Init a game

--- src/main.js
+++ src/main.js
@@ -9,6 +9,16 @@ new Vue({
     times: [
       new Time("America MG", require('./assets/america_mg_60x60.png')),
       new Time("Atletico PR", require('./assets/atletico-pr_60x60.png')),
-    ]
+    ],
+    novoJogo: {
+      casa: {
+          time: null,
+          gols: 0
+      },
+      fora: {
+          time: null,
+          gols: 0
+      },
+    }
   }
 })
